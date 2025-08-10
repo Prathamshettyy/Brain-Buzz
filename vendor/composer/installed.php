8061a8ad522d302265ef516d9c489df95f1cffc0
prathamshettyy <prathamr.cs22@sahyadri.edu.in>
2025-08-10 20:46:49 +0530

    fix: Update reference hashes in installed.php for consistency

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'fd667e94568f238816624042cc8d3090bae8e097',
+        'reference' => 'b6508628b10adf3660fe8e0b13a68899948d36c7',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'fd667e94568f238816624042cc8d3090bae8e097',
+            'reference' => 'b6508628b10adf3660fe8e0b13a68899948d36c7',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
